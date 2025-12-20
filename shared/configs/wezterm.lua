9c7bb67ac45348bc08af0095f8e1b16635121782
Caesar Balona <caesar.balona@gmail.com>
2025-12-20 10:07:40 +0200

    refactor WezTerm configuration for consistency and readability

--- shared/configs/wezterm.lua
+++ shared/configs/wezterm.lua
@@ -1,25 +1,32 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- SHELL
+config.default_prog = { "pwsh.exe" }
+
+-- KEYBINDINGS
+config.keys = {
+	{ key = "l", mods = "CTRL|SHIFT", action = wezterm.action.ShowLauncher },
+}
+
 -- APPEARANCE
-config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 11.0
 
 -- WINDOW
 config.window_background_opacity = 0.8
-config.win32_system_backdrop = 'Acrylic'
-config.window_decorations = 'RESIZE'
+config.win32_system_backdrop = "Acrylic"
+config.window_decorations = "RESIZE"
 config.window_padding = {
-  left = 10,
-  right = 10,
-  top = 10,
-  bottom = 10,
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
 }
 
 -- INTERACTION
-
-config.selection_word_boundary = ' \t\n{}[]()<>:;.,-+*/=%"\''
+config.selection_word_boundary = " \t\n{}[]()<>:;.,-+*/=%\"'"
 config.scrollback_lines = 10000
 
 return config
